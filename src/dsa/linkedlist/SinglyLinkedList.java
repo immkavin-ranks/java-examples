80b921f1b1fba20c930b8580c0de8e27537902de
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-09-22 16:42:51 +0530

    insert, search, traverse, delete in singly linked list

--- src/dsa/linkedlist/SinglyLinkedList.java
+++ src/dsa/linkedlist/SinglyLinkedList.java
@@ -4,8 +4,7 @@ public class SinglyLinkedList {
     public Node head;
     public Node tail;
     public int size;
-
-    public void createSinglyLinkedList(int nodeValue) {
+    public void create(int nodeValue) {
         Node node = new Node();
         node.next = null;
         node.value = nodeValue;
@@ -14,5 +13,92 @@ public class SinglyLinkedList {
         tail = node;
 
         size = 1;
+    }
+    public void insert(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            create(nodeValue);
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public void traverse() {
+        if (head == null) return;
+        Node tempNode = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(tempNode.value);
+            if (i < size - 1) {
+                System.out.print(" -> ");
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.println();
+    }
+
+    public void search(int nodeValue) {
+        if (head == null) return;
+        Node tempNode = head;
+        for (int i = 0; i < size; i++) {
+            if (tempNode.value == nodeValue) {
+                System.out.println("Found the node at location, i = " + i);
+                return;
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.println("Node not found!");
+    }
+
+    public void deleteNode(int location) {
+        if (head == null) return;
+        if (location == 0) {
+            head = head.next;
+            size--;
+            // if deletion in singly linked list with only one node
+            if (size == 0) tail = null;
+        } else if (location >= size - 1) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 2; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {
+                head = tail = null;
+                size--;
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+        }
+    }
+
+    public void delete() {
+        head = tail = null;
+        size = 0;
     }
 }
--- src/dsa/linkedlist/SinglyLinkedListDemo.java
+++ src/dsa/linkedlist/SinglyLinkedListDemo.java
@@ -3,7 +3,22 @@ package dsa.linkedlist;
 public class SinglyLinkedListDemo {
     public static void main(String[] args) {
         SinglyLinkedList sLL = new SinglyLinkedList();
-        sLL.createSinglyLinkedList(5);
+        sLL.create(5);
         System.out.println(sLL.head.value);
+        sLL.insert(6, 1);
+        sLL.insert(7, 0);
+        System.out.println(sLL.head.next.value);
+        System.out.println(sLL.head.next.next.value);
+
+        sLL.insert(8, 4);
+        sLL.traverse();
+        sLL.search(6);
+        sLL.search(10);
+        sLL.deleteNode(3);
+        sLL.traverse();
+        System.out.println(sLL.tail.value);
+
+        sLL.delete();
+        System.out.println(sLL.size);
     }
 }
